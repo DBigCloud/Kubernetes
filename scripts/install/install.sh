363b6d75a62c63e76e5dddbc57d5422ba6667139
drsromero <daniel.romero@ilusia.com>
2020-02-03 10:11:32 +0100

    Corrected node creation home folder

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -75,12 +75,11 @@ then
 	sudo swapoff -a
 	sudo hostnamectl set-hostname kubernetes-master
 	sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+    #Set up local kubeconfig:
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
 fi
-
-#Set up local kubeconfig:
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 #Apply Flannel CNI network overlay:
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
